af130288d00eca3159fad16fff3bb9d40c0a9e85
araltiparmak <aliriza_altiparmak@hotmail.com>
2024-06-09 12:46:22 +0200

    - refactoring

--- s3-static-site/s3.tf
+++ s3-static-site/s3.tf
@@ -2,14 +2,27 @@ resource "aws_s3_bucket" "website_bucket" {
   bucket = var.domain_name
 }
 
-resource "aws_s3_bucket_acl" "website_bucket_acl" {
+resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
   bucket = aws_s3_bucket.website_bucket.id
-  acl    = var.s3_acl
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.website_bucket.id
-  policy = data.aws_iam_policy_document.website_iam_policy.json
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.bucket_access_block]
+}
+
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  bucket     = aws_s3_bucket.website_bucket.id
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
@@ -22,4 +35,24 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   error_document {
     key = "error.html"
   }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.website_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
